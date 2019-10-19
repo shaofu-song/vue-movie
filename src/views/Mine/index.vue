08a0ae46f8eb849c36c9ae1383fe22a404a9da2a
shaofu-song <3478162259@qq.com>
2019-10-19 23:57:26 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="main">
+      <Header title="我的小米"/>
+        <div id="content">
+          <Login />
+        </div>
+      <TarBar />
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header';
+import TarBar from '@/components/TarBar';
+import Login from '@/components/Login';
+
+
+export default {
+  name: 'Mine',
+  components: {
+    Header,
+    TarBar,
+    Login
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
